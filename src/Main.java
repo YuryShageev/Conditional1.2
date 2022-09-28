d3705c33ce182edd08b3fe034a2f1b0cf02acd64
YuryShageev <schageev.yur@yandex.ru>
2022-09-28 15:37:52 +0300

    Третий блок домашки

--- src/Main.java
+++ src/Main.java
@@ -24,5 +24,53 @@ public class Main {
         } else {
             System.out.println("Если возраст человека не больше 24, то ему не нужно ходить на работу");
         }
+
+        //Задание 2
+        System.out.println("Task 2");
+
+        int child = 12;
+        boolean canNotRide = child < 5;
+        boolean canRideWithParent = child >= 5 || child <= 14;
+        boolean canRideAlone = child > 14;
+        if (canNotRide) {
+            System.out.println("Нельзя кататься! Ещё мал!");
+        } else {
+            System.out.println("Можно кататься");
+        }
+        if (canRideWithParent) {
+            System.out.println("только в сопровождении родителя");
+        } else {
+            System.out.println("Сколько человеку лет?");
+        }
+        if (canRideAlone) {
+            System.out.println("Можете свободно кататься");
+        } else {
+            System.out.println("Сколько человеку лет?");
+        }
+
+        // задание 3
+        System.out.println("Task 3");
+
+        int one = 1;
+        int two = 2;
+        int three = 3;
+        boolean notBigger = one > two && one > three;
+        boolean stillNotBigger = two > three;
+        boolean biggest = three > two;
+        if (biggest) {
+            System.out.println("Число three больше, чем two или one");
+        } else {
+            System.out.println("Число three не больше, чем two или one");
+        }
+        if (stillNotBigger) {
+            System.out.println("Число two больше, чем three или one");
+        } else {
+            System.out.println("Число two не больше, чем three");
+        }
+        if (notBigger) {
+            System.out.println("Число one больше, чем two или three");
+        } else {
+            System.out.println("Число one не больше, чем two или three");
+        }
     }
 }
